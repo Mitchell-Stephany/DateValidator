dc19f3293e4dbae91422593f5cc0df885d9e4fa8
Mitchell <35747185+Mitchell-Stephany@users.noreply.github.com>
2021-10-04 21:36:30 -0500

    finished code

--- DateValidation/DateValidator.cpp
+++ DateValidation/DateValidator.cpp
@@ -14,108 +14,57 @@
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
 	cout<<"Enter numeric values for the current date [mm dd yyyy] -> ";
 	int month; int day; int year;
 	cin >> month; cin >> day; cin >> year;
-	string newMonth;
 	int leapYear = 0;
-
-	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
-	{
+	string newMonth;
+	string months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+	for (int i; i <= 11; i++){
+		if ((month - 1) == i){
+			newMonth = months[i];
+		}
+	}
+	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)){
 		leapYear = 1;
 	}
-	if(month == 1)
-	{
-		newMonth = "January";
-	}
-	else if(month == 3)
-	{
-		newMonth = "March";
-	}
-	else if (month == 4)
-	{
-		newMonth = "April";
-	}
-	else if(month == 5)
-	{
-		newMonth = "May";
-	}
-	else if (month == 6)
-	{
-		newMonth = "June";
-	}
-	else if (month == 7)
-	{
-		newMonth = "July";
-	}
-	else if (month == 8)
-	{
-		newMonth = "August";
-	}
-	else if (month == 9)
-	{
-		newMonth = "September";
-	}
-	else if(month == 10)
-	{
-		newMonth = "October";
-	}
-	else if (month == 11)
-	{
-		newMonth = "November";
-	}
-	else if(month == 12)
-	{
-		newMonth = "December";
-	}
-	if (month < 1 || month > 12)
-	{
+	if (month < 1 || month > 12){
 		cout << month << "/" << day << "/" << year << " is not a date; " << "The month must be in the interval [1,12].";
 	}
-	else if (day < 1 || day >31)
-	{
+	else if (day < 1 || day >31){
 		cout << month << "/" << day << "/" << year << " is not a date; " << "The day must be in the interval [1,31].";
 	}
-	else if (year < 1583)
-	{
+	else if (year < 1583){
 		cout << month << "/" << day << "/" << year << " is not a date; " << "The year must come after 1582.";
 	}
-	else if(month == 2 && day > 29)
-	{
-		cout << month << "/" << day << "/" << year << " is not a valid date; " << "The day in February cannot be greater than 29.";
+	else if(month == 2 && day > 29){
+		cout << month << "/" << day << "/" << year << " is not a date; " << "The day in February cannot be greater than 29.";
 	}
-	else if (month == 2 && day == 29 && leapYear != 1)
-	{
-		cout<<"February cannot have 29 days in a common year.";
+	else if (month == 2 && day == 29 && leapYear != 1){
+		cout << month << "/" << day << "/" << year << " is not a date; February cannot have 29 days in a common year.";
 	}
-	else if (month == 2 && day <= 29)
-	{
-		if (leapYear == 1)
-		{
+	else if (month == 2 && day <= 29){
+		if (leapYear == 1){
 			cout << "The date February " << day << ", " << year << " is in a leap year." << endl;
 		}
-		else
-		{
+		else{
 			cout  << "The date February " << day << ", " << year << " is in a common year." << endl;
 		}
 	}
-	else if ((month == 4 || month == 6 || month == 9 || month == 11) && (day == 31))
-	{
-		cout << month << "/" << day << "/" << year << " is not a valid date. The day in " << newMonth << " cannot be 31.";
+	else if ((month == 4 || month == 6 || month == 9 || month == 11) && (day == 31)){
+		cout << month << "/" << day << "/" << year << " is not a date. The day in " << newMonth << " cannot be 31.";
 	}
-	else if (leapYear == 1)
-	{
+	else if (leapYear == 1){
 		cout << "The date " << newMonth << " " << day << ", " << year << " is in a leap year.";
 	}
-	else if (leapYear == 0)
-	{
+	else if (leapYear == 0){
 		cout << "The date " << newMonth << " " << day << ", " << year << " is in a common year.";
 	}
 	return 0;
-	}
-
+}
 
 
 
